0dfb2552f742632d8245526808dfccf06cf1b740
Tarik <tarik@yieldmo.com>
2016-01-14 17:53:27 +0000

    Diff now differences since local HEAD

--- lib/branch.sh
+++ lib/branch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
-FILES="$(git diff $(git merge-base origin/HEAD HEAD).. --name-only)"
+#FILES="$(git diff $(git merge-base origin/HEAD HEAD).. --name-only)"
+FILES="$(git diff --name-only)"
 if [ -n "$FILES" ]; then
   echo "$FILES" | sort -u | xargs find 2> /dev/null
 fi
